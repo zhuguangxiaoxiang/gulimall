f2d563af1e169da259d130aab7add8cdad5c80cf
liujing <2870640960@qq.com>
2023-05-08 23:54:21 +0800

    品牌管理-OSS获取服务端签名的完成

--- gulimall-product/src/test/java/com/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/gulimall/product/GulimallProductApplicationTests.java
@@ -1,9 +1,13 @@
 package com.gulimall.product;
 
+import com.aliyun.oss.*;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.PutObjectResult;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.additional.query.impl.QueryChainWrapper;
 import com.gulimall.product.entity.BrandEntity;
 import com.gulimall.product.service.BrandService;
+import com.netflix.client.ClientException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +15,68 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
+/**
+ * 1、引入oss-starter
+ * 2、配置key，endpoit相关信息即可
+ * 3、使用OSSClient进行相关操作
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GulimallProductApplicationTests {
 
-    @Resource
+    @Autowired
     BrandService brandService;
+
+    @Resource
+    OSSClient ossClient;
+
+
+    @Test
+    public void testUpload() throws FileNotFoundException {
+        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+//        String endpoint = "oss-cn-beijing.aliyuncs.com";
+//        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+//        String accessKeyId = "LTAI5t7xs4DrWEipFbh7FvjW";
+//        String accessKeySecret = "U4bqVCyrrqloPqBh6PXvALgYhvCWWU";
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "gulimall-2870";
+        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+        String objectName = "1601228436zFk8.jpg";
+        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
+        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        String filePath= "F:\\Administrator\\图片\\__www.2meinv.com\\2020好看的美女福利妹子图片集每日更新第24期\\1601228436zFk8.jpg";
+
+        // 创建OSSClient实例。
+//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        try {
+            InputStream inputStream = new FileInputStream(filePath);
+            // 创建PutObjectRequest对象。
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, inputStream);
+            // 设置该属性可以返回response。如果不设置，则返回的response为空。
+            putObjectRequest.setProcess("true");
+            // 创建PutObject请求。
+            PutObjectResult result = ossClient.putObject(putObjectRequest);
+            // 如果上传成功，则返回200。
+            System.out.println(result.getResponse().getStatusCode());
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+    }
     @Test
     public void contextLoads() {
 
